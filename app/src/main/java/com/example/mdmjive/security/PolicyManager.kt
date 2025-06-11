6d3b509de716b279c42cd5e61e4a14c2b5a3e365
BisonByte <blackshainorg@gmail.com>
2025-06-10 20:27:27 -0400

    Add device restrictions and update README

--- app/src/main/java/com/example/mdmjive/security/PolicyManager.kt
+++ app/src/main/java/com/example/mdmjive/security/PolicyManager.kt
@@ -60,4 +60,47 @@ class PolicyManager(private val context: Context) {
             Log.e("MDM", "Error configurando timeout: ${e.message}")
         }
     }
+
+    fun disableFactoryReset() {
+        try {
+            dpm.addUserRestriction(componentName, android.os.UserManager.DISALLOW_FACTORY_RESET)
+            Log.d("MDM", "Restablecimiento de fábrica deshabilitado")
+        } catch (e: Exception) {
+            Log.e("MDM", "Error deshabilitando restablecimiento: ${e.message}")
+        }
+    }
+
+    fun lockBrightness() {
+        try {
+            dpm.setSystemSetting(componentName, android.provider.Settings.System.SCREEN_BRIGHTNESS, "255")
+            dpm.setSystemSetting(componentName, android.provider.Settings.System.SCREEN_BRIGHTNESS_MODE,
+                android.provider.Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL.toString())
+            dpm.addUserRestriction(componentName, android.os.UserManager.DISALLOW_CONFIG_BRIGHTNESS)
+            Log.d("MDM", "Brillo fijado al 100% y bloqueado")
+        } catch (e: Exception) {
+            Log.e("MDM", "Error configurando brillo: ${e.message}")
+        }
+    }
+
+    fun lockGPS() {
+        try {
+            dpm.setSecureSetting(componentName, android.provider.Settings.Secure.LOCATION_MODE,
+                android.provider.Settings.Secure.LOCATION_MODE_HIGH_ACCURACY.toString())
+            dpm.addUserRestriction(componentName, android.os.UserManager.DISALLOW_CONFIG_LOCATION)
+            Log.d("MDM", "GPS forzado y bloqueado")
+        } catch (e: Exception) {
+            Log.e("MDM", "Error configurando GPS: ${e.message}")
+        }
+    }
+
+    fun lockDateTime() {
+        try {
+            dpm.setGlobalSetting(componentName, android.provider.Settings.Global.AUTO_TIME, "1")
+            dpm.setGlobalSetting(componentName, android.provider.Settings.Global.AUTO_TIME_ZONE, "1")
+            dpm.addUserRestriction(componentName, android.os.UserManager.DISALLOW_CONFIG_DATE_TIME)
+            Log.d("MDM", "Fecha y hora configuradas automáticamente y bloqueadas")
+        } catch (e: Exception) {
+            Log.e("MDM", "Error configurando fecha/hora: ${e.message}")
+        }
+    }
 }
