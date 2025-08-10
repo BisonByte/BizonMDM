4dac274e9ed9ede58a9352b6a1dc2f48342b6558
BisonByte <blackshainorg@gmail.com>
2025-08-10 00:42:59 -0400

    Parse FCM data and execute commands

--- mobile/app/src/main/java/com/example/mdmjive/services/FCMService.kt
+++ mobile/app/src/main/java/com/example/mdmjive/services/FCMService.kt
@@ -3,15 +3,12 @@ package com.example.mdmjive.services
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import android.util.Log
-import com.example.mdmjive.BuildConfig
-import com.example.mdmjive.repository.DeviceRepository
-import com.example.mdmjive.network.ApiServiceFactory
-import com.example.mdmjive.database.LogDatabase
 import com.example.mdmjive.controls.CommandExecutor
+import com.example.mdmjive.network.models.Command
+import com.example.mdmjive.utils.TokenManager
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import com.example.mdmjive.utils.TokenManager
 
 class FCMService : FirebaseMessagingService() {
     override fun onNewToken(token: String) {
@@ -23,13 +20,16 @@ class FCMService : FirebaseMessagingService() {
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
         CoroutineScope(Dispatchers.IO).launch {
-            val repository = DeviceRepository(
-                ApiServiceFactory.create(BuildConfig.BASE_URL),
-                LogDatabase.getDatabase(applicationContext).deviceDao()
-            )
-            val commands = repository.fetchCommands(applicationContext)
-            if (commands.isNotEmpty()) {
-                CommandExecutor(applicationContext).execute(commands)
+            val action = message.data["action"]
+            if (action != null) {
+                val command = Command(
+                    action = action,
+                    packageName = message.data["packageName"],
+                    message = message.data["message"]
+                )
+                CommandExecutor(applicationContext).execute(listOf(command))
+            } else {
+                Log.e("FCMService", "Acción no proporcionada en el mensaje FCM")
             }
         }
     }
